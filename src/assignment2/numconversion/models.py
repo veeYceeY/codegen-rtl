fde1629c07d4e5c59a92062cd6e8f08ea0d85eda
veeYceeY <veeyceey@gmail.com>
2021-09-24 10:23:21 +0530

    Delete src/assignment2 directory
    un necessary commit

--- src/assignment2/numconversion/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from audiofield.fields import AudioField
-# Create your models here.
-class converted(models.Model):
-    audio=AudioField(upload_to="", blank=True,ext_whitelist=(".mp3", ".wav", ".ogg"),help_text=("Allowed type - .mp3, .wav, .ogg"))
